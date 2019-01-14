54ca11d7e19da67301b3167d41ec41d3be3e0aa9
Pontus Hansson <pontexzor@live.se>
2019-01-14 10:57:20 +0100

    added Example code

---
+++ Example/build/precache-manifest.ca9cb29fc0902b52b4267873ed779456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba0e4dbe4666e1e0acb9",
+    "url": "/static/js/main.ba0e4dbe.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "ba0e4dbe4666e1e0acb9",
+    "url": "/static/css/main.13e7f0a6.chunk.css"
+  },
+  {
+    "revision": "cc71406591a1f1cba14ae3c40f3182d3",
+    "url": "/index.html"
+  }
+];
